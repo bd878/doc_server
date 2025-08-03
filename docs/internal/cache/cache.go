591041a2c0dc075f93b7ede7b7a86cf5101cd997
bd878 <slavad878@gmail.com>
2025-08-03 21:24:20 +0300

    free, remove from cache

--- docs/internal/cache/cache.go
+++ docs/internal/cache/cache.go
@@ -69,15 +69,100 @@ func (c *Cache) Get(id, login string) (meta *docs.Meta) {
 func (c *Cache) Remove(id string) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
+
+	logins, ok := c.idToLogin[id]
+	if !ok {
+		return
+	}
+
+	for _, login := range logins {
+		metas, ok := c.loginToMeta[login]
+		if !ok {
+			// no cache for given login
+			continue
+		}
+
+		var i int
+		var found bool
+		var meta *docs.Meta
+		// find index to drop element from metas slice
+		for i, meta = range metas {
+			if meta.ID == id {
+				found = true
+				break
+			}
+		}
+
+		// it must be found, though...
+		if found {
+			switch {
+			case len(metas) == 1:
+				metas = make([]*docs.Meta, 0)
+			case i < (len(metas)-1):
+				// move last to found index
+				metas[i] = metas[len(metas)-1]
+				metas = metas[:len(metas)-1]
+			case i == len(metas)-1:
+				metas = metas[:len(metas)-1]
+			}
+		}
+
+		c.loginToMeta[login] = metas
+	}
+
+	delete(c.idToLogin, id)
 }
 
 func (c *Cache) ListOwner(owner, key, value string, limit int) (docs []*docs.Meta) {
+	// not implemented
 	return nil
 }
 
 func (c *Cache) ListLogin(login, key, value string, limit int) (docs []*docs.Meta) {
+	// not implemented
 	return nil
 }
 
-func (c *Cache) Free(login string) {
+func (c *Cache) Free(owner string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	metas, ok := c.loginToMeta[owner]
+	if !ok {
+		return
+	}
+
+	for _, meta := range metas {
+		logins, ok := c.idToLogin[meta.ID]
+		if !ok {
+			continue
+		}
+
+		var i int
+		var found bool
+		var login string
+		// find index to drop element from logins slice
+		for i, login = range logins {
+			if login == owner {
+				found = true
+				break
+			}
+		}
+
+		if found {
+			switch {
+			case len(logins) == 1:
+				logins = make([]string, 0)
+			case i < (len(logins)-1):
+				logins[i] = logins[len(logins)-1]
+				logins = logins[:len(logins)-1]
+			case i == len(logins)-1:
+				logins = logins[:len(logins)-1]
+			}
+		}
+
+		c.idToLogin[meta.ID] = logins
+	}
+
+	delete(c.loginToMeta, owner)
 }
