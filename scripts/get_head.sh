e8bad4605c96babb5543bed608ba1b04deed2749
bd878 <slavad878@gmail.com>
2025-08-04 10:51:26 +0300

    get_head -I

--- scripts/get_head.sh
+++ scripts/get_head.sh
@@ -5,4 +5,4 @@
 token=${1?:"Usage: get_head.sh token id"}
 id=${2?:"Usage: get_head.sh token id"}
 
-curl -v -XHEAD "http://138.124.107.242:80/api/docs/$id?token=$token"
+curl -I "http://138.124.107.242:80/api/docs/$id?token=$token"
